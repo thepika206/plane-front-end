1f41af182d402550654e4c8a380201fcd3a875b8
mrunlockedtech <jrbhrn@gmail.com>
2022-10-21 13:20:08 -0500

    Add basic title and slogan

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,11 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1 id='landingTitle'>Plan.e</h1>
+      <h3>Making sure you never have to plan a trip alone again </h3>
+      <br/>
+      <h2>Hello, {user ? user.name : 'friend'}</h2>
+
     </main>
   )
 }
