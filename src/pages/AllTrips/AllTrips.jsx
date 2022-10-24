db1aabc657fdf8c22866e40513eb044f6fa748ff
Jenn <jenniferjuarezjj@gmail.com>
2022-10-24 15:02:05 -0400

    css for all trips list

--- src/pages/AllTrips/AllTrips.jsx
+++ src/pages/AllTrips/AllTrips.jsx
@@ -1,15 +1,31 @@
+import { Link } from 'react-router-dom';
 import styles from './AllTrips.module.css'
+import ActivityCard from '../../components/ActivityCard/ActivityCard';
 
 const AllTrips = (props) => {
   return (
-    <main className={styles.container}>
-      {props.trips?.map((trip) => (
-        <p key={trip._id}>
-          {trip.name}
-        </p>
-      ))}
-    </main>
-  )
+    <div className={styles.allTrips}>
+      <h1 className={styles.allTripsTitle}>All Trips</h1>
+      <div className="">
+        
+      </div>
+          
+
+      <div>
+        <main className={styles.container}>
+          {props.trips.map(activity =>
+
+            <ActivityCard key={activity._id} activity={activity} />
+
+          )}
+           
+
+        </main>
+      </div>
+
+    </div>
+  );
 }
 
 export default AllTrips
+    
