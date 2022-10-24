3b26390ceb74db3e7b6879a41d8b543d46cc5555
mrunlockedtech <jrbhrn@gmail.com>
2022-10-24 13:02:01 -0500

    Fix activity plan key issue and move card to components directory

--- src/pages/TripDetails/TripDetails.jsx
+++ src/pages/TripDetails/TripDetails.jsx
@@ -1,43 +1,43 @@
 import styles from './TripDetails.module.css'
-import ActivityPlanCard from '../ActivityPlanCard/ActivityPlanCard'
+import ActivityPlanCard from '../../components/ActivityPlanCard/ActivityPlanCard'
 import * as tripService from '../../services/tripService'
 import { useParams, Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
 const TripDetails = (props) => {
   const { id } = useParams()
-  const [trip,setTrip] = useState({})
+  const [trip, setTrip] = useState({})
 
-useEffect(() => {
-  const fetchTrip = async() => {
-    const tripData = await tripService.show(id)
-    setTrip(tripData)
-  }
-  fetchTrip()
-},[id])
+  useEffect(() => {
+    const fetchTrip = async () => {
+      const tripData = await tripService.show(id)
+      setTrip(tripData)
+    }
+    fetchTrip()
+  }, [id])
 
-  return ( 
-  <div className={styles.tripDetails}>
-    <div className={styles.topRow}>
-      <p>Trips {'>'} {trip.name} </p>
-      <div className={styles.titleCard}>
-        <h1>{trip.name}</h1>
-        <h2>{trip.startDate} {' - '} {trip.endDate}</h2>
-        <h3>{trip.private ? 'Private': ''}</h3>
-        <Link to={`/trips/${id}/edit`} className="btn btn-warning">Edit Trip</Link>
+  return (
+    <div className={styles.tripDetails}>
+      <div className={styles.topRow}>
+        <p>Trips {'>'} {trip.name} </p>
+        <div className={styles.titleCard}>
+          <h1>{trip.name}</h1>
+          <h2>{trip.startDate} {' - '} {trip.endDate}</h2>
+          <h3>{trip.private ? 'Private' : ''}</h3>
+          <Link to={`/trips/${id}/edit`} className="btn btn-warning">Edit Trip</Link>
+        </div>
       </div>
-    </div>
-    <div>
+      <div>
         <main className={styles.container}>
-          {trip.activityPlans?.map((activityPlan,idx) =>
+          {trip.activityPlans?.map((activityPlan, idx) =>
 
-            <ActivityPlanCard key={idx} activityPlan={activityPlan} activities={props.activities}/>
+            <ActivityPlanCard key={idx} activityPlan={activityPlan} activities={props.activities} />
 
           )}
 
         </main>
       </div>
-  </div> );
+    </div>);
 }
- 
+
 export default TripDetails;
